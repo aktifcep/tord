7868216d30d9b6288d387fef6a74c10d604d1099
brett.gentoo <brett.gentoo@9255654f-4652-1c84-a8f9-fee883ebe1f1>
2010-06-27 20:48:11 +0000

    brett.gentoo@googlemail.com --------------------------- Added default.properties AndroidManifest.xml res res/values res/values/strings.xml res/drawable-hdpi res/drawable-hdpi/icon.png res/drawable-ldpi res/drawable-ldpi/icon.png res/drawable-mdpi res/drawable-mdpi/icon.png res/layout res/layout/main.xml
    Started sorting out layout of player input screen
    
    TODO :
    
    LOADS

--- src/uk/co/royles/tord/tord.java
+++ src/uk/co/royles/tord/tord.java
@@ -2,15 +2,13 @@ package uk.co.royles.tord;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.TextView;
 
 public class tord extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        TextView tv = new TextView(this);
-        tv.setText("Truth or Dare");
-        setContentView(tv);
+        setContentView(R.layout.main);
+
     }
 }
