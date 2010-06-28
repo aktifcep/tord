da9175adc0d9fd4cc671bc6f5ffd3438f717db63
brett.gentoo <brett.gentoo@9255654f-4652-1c84-a8f9-fee883ebe1f1>
2010-06-28 19:27:34 +0000

    added loads of stuff

--- trunk/src/uk/co/royles/tord/tord.java
+++ trunk/src/uk/co/royles/tord/tord.java
@@ -17,15 +17,44 @@
  */
 package uk.co.royles.tord;
 
-import android.app.Activity;
+import android.app.TabActivity;
+import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.widget.TabHost;
 
-public class tord extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+public class tord extends TabActivity {
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.main);
 
-    }
+	    Resources res = getResources(); // Resource object to get Drawables
+	    TabHost tabHost = getTabHost();  // The activity TabHost
+	    TabHost.TabSpec spec;  // Resusable TabSpec for each tab
+	    Intent intent;  // Reusable Intent for each tab
+
+	    // Create an Intent to launch an Activity for the tab (to be reused)
+	    intent = new Intent().setClass(this, TruthsActivity.class);
+
+	    // Initialize a TabSpec for each tab and add it to the TabHost
+	    spec = tabHost.newTabSpec("players").setIndicator("Players",
+	                      res.getDrawable(R.drawable.ic_tab_players))
+	                  .setContent(intent);
+	    tabHost.addTab(spec);
+
+	    // Do the same for the other tabs
+	    intent = new Intent().setClass(this, DaresActivity.class);
+	    spec = tabHost.newTabSpec("dares").setIndicator("Dares",
+	                      res.getDrawable(R.drawable.ic_tab_dares))
+	                  .setContent(intent);
+	    tabHost.addTab(spec);
+
+	    intent = new Intent().setClass(this, PlayersActivity.class);
+	    spec = tabHost.newTabSpec("truths").setIndicator("Truths",
+	                      res.getDrawable(R.drawable.ic_tab_truths))
+	                  .setContent(intent);
+	    tabHost.addTab(spec);
+
+	    tabHost.setCurrentTab(1);
+	}
 }
